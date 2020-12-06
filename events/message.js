b6de6e5f17f3460857b5539df44eced4eb4c7582
Glitch (noon-erratic-iris) <none>
2020-12-06 17:17:46 +0000

    🐷🎊 Checkpoint ./events/message.js:7380171/58

--- events/message.js
+++ events/message.js
@@ -55,6 +55,7 @@ module.exports = async (client, msg) => {
               const embed = new Discord.RichEmbed()
     .setAuthor(`TICKET OPENED`)
     .setDescription(`Greetings ${msg.author.username}, \n\n Your ticket has been sent to Bloxway Support staff! \n\n The support staff will reply to this ticket in a delay of 1-4 hours. \n\n\n *Regards*\n*Bloxway Support Team*`)
+    .addField(`Content`, msg.content)
     .setColor(tokens.generic.colour.default)
     .setTimestamp()
     .setFooter(`Bloxway Support System | Made by opxrator#0001 | Reply below to answer`)
